ed38d9e1a5eced4fdf4a551c81a2c9d9da9aed93
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-26 11:16:23 +0100

    add change name file postsController

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
-
-
 app.use(express.json());
-
 
 const postsRouter = require("./routers/posts");
 
-
 app.use("/posts", postsRouter);
 
-
-app.listen(port, () => {
-    console.log(`Server avviato su http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log("Server avviato su http://localhost:3000");
 });
