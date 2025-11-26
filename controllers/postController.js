05cd64f3f27a84af42179f4bdd8824e3e5fee70a
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-26 11:05:37 +0100

    add show function

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,3 +3,14 @@ const posts = require("../posts");
 function index(req, res) {
     res.json(posts);
 }
+
+function show(req, res) {
+    const { id } = req.params;
+    const post = posts.find(p => p.id === parseInt(id));
+
+    if (!post) {
+        return res.status(404).json({ error: "Post non trovato" });
+    }
+
+    res.json(post);
+}
--- routers/posts.js
+++ routers/posts.js
@@ -6,16 +6,6 @@ const router = express.Router();
 
 
 
-router.get("/:id", (req, res) => {
-    const { id } = req.params;
-    const post = posts.find(p => p.id === parseInt(id));
-
-    if (!post) {
-        return res.status(404).json({ error: "Post non trovato" });
-    }
-
-    res.json(post);
-});
 
 router.get("/", (req, res) => {
     res.send("Lista dei post");
