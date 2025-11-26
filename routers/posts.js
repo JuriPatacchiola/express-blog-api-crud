5fbf0cdd884899a4eaab6e33aea4314223ec6530
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-26 11:09:08 +0100

    add the const with all export element controller

--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 
-
+const {
+    index,
+    show,
+    store,
+    update,
+    destroy
+} = require("../controllers/postsController");
 
 
 module.exports = router;
