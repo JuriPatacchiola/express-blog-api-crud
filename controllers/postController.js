62753f65c0b4cc8489e5769d93bd7def9d5a6ece
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-26 11:08:02 +0100

    add delate in controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,3 +24,20 @@ function update(req, res) {
     const { id } = req.params;
     res.send(`Aggiornamento del post ${id}`);
 }
+
+
+function destroy(req, res) {
+    const { id } = req.params;
+    const index = posts.findIndex(p => p.id === parseInt(id));
+
+    if (index === -1) {
+        return res.status(404).json({ error: "Post non trovato" });
+    }
+
+    const deletedPost = posts.splice(index, 1);
+
+    res.json({
+        message: "Post eliminato con successo",
+        deleted: deletedPost[0]
+    });
+}
--- routers/posts.js
+++ routers/posts.js
@@ -5,22 +5,4 @@ const router = express.Router();
 
 
 
-
-router.delete("/:id", (req, res) => {
-    const { id } = req.params;
-    const index = posts.findIndex(p => p.id === parseInt(id));
-
-    if (index === -1) {
-        return res.status(404).json({ error: "Post non trovato" });
-    }
-
-    const deletedPost = posts.splice(index, 1);
-
-    res.json({
-        message: "Post eliminato con successo",
-        deleted: deletedPost[0]
-    });
-});
-
-
 module.exports = router;
